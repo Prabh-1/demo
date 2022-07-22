25f4c2fd1e42d9425858b3c53bbf28c3a3bda37a
Prabhdeep Kaur <prabhdeep2015105@gndec.ac.in>
2022-07-22 10:56:32 +0530

    string programs and conversions

---
+++ day 1.py
@@ -0,0 +1,79 @@
+# string programs
+print('hello')
+a='python'
+print(a)
+print(a[0:])
+print(a[1:])
+print(a[2:4])
+print(a[::-1])
+print(a[:-1])
+print(a[:-2])
+print(a[-1:])
+print(a[-3:])
+print(a.isupper())
+print(a.islower())
+print(a.upper())
+print(a.capitalize())
+print(a.casefold())
+print(a.center(15,'*'))
+print(a.encode())
+print(a.endswith('n'))
+print(a.expandtabs())
+print(a.find('k'))
+print(a.index('h'))
+print(a.isalnum())
+print(a.isalpha())
+print(a.isdecimal())
+print(a.isdigit())
+print(a.partition('th'))
+
+# conversions of data types 
+# integer operations
+a=10
+print(a)
+print(type(a))#print data type
+print(id(a))
+print(float(a))#convert to float
+print(bool(a))#return bool value
+print(complex(a))#conert to complex
+print(str(a))#convert to string
+
+# float value
+a=23.56
+print(a)
+print(type(a))#print data type
+print(id(a))
+print(int(a))#convert to int
+print(bool(a))#return bool value
+print(complex(a))#conert to complex
+print(str(a))#convert to string
+
+# string
+a='hello'
+print(a)
+print(type(a))#print data type
+print(id(a))
+#print(int(a))#convert to int(error if it is not a number)
+print(bool(a))#return bool value
+# print(complex(a))#conert to complex(error)
+# print(float(a))#convert to float(error)
+
+# complex number
+a=23.56+0j
+print(a)
+print(type(a))#print data type
+print(id(a))
+# print(int(a))#convert to int
+print(bool(a))#return bool value
+# print(float(a))#conert to float
+print(str(a))#convert to string
+
+# bool 
+a=True
+print(a)
+print(type(a))#print data type
+print(id(a))
+print(int(a))#convert to int
+print(float(a))#convert to float
+print(complex(a))#conert to complex
+print(str(a))#convert to string
