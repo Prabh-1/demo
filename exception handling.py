5f12603b29e897eec6196b7e4fe666a0c4cde182
Prabhdeep Kaur <prabhdeep2015105@gndec.ac.in>
2022-08-14 21:31:16 +0530

    comment the ques

--- exception handling.py
+++ exception handling.py
@@ -1,53 +1,53 @@
 # Program to handle multiple errors with one except statement 
 
-# def fun(a):
-# 	if a < 4:
+def fun(a):
+	if a < 4:
 
-# 		# throws ZeroDivisionError for a = 3
-# 		b = a/(a-3)
+		# throws ZeroDivisionError for a = 3
+		b = a/(a-3)
 
-# 	# throws NameError if a >= 4
-# 	print("Value of b = ", b)
+	# throws NameError if a >= 4
+	print("Value of b = ", b)
 	
-# try:
-#     fun(2)
-#     fun(3)
-# # note that braces () are necessary here for
-# # multiple exceptions
-# except ZeroDivisionError:
-# 	print("ZeroDivisionError Occurred and Handled")
-# except NameError:
-# 	print("NameError Occurred and Handled")
-# fun(1)
+try:
+    fun(2)
+    fun(3)
+# note that braces () are necessary here for
+# multiple exceptions
+except ZeroDivisionError:
+	print("ZeroDivisionError Occurred and Handled")
+except NameError:
+	print("NameError Occurred and Handled")
+fun(1)
 
 # Program to depict else clause with try-except
 # Function which returns a/b
-# def AbyB(a , b):
-# 	try:
-# 		c = ((a+b) / (a-b))
-# 	except ZeroDivisionError:
-# 		print ("a/b result in 0")
-# 	else:
-# 		print (c)
+def AbyB(a , b):
+	try:
+		c = ((a+b) / (a-b))
+	except ZeroDivisionError:
+		print ("a/b result in 0")
+	else:
+		print (c)
 
-# # Driver program to test above function
-# AbyB(2.0, 3.0)
-# AbyB(3.0, 3.0)
+# Driver program to test above function
+AbyB(2.0, 3.0)
+AbyB(3.0, 3.0)
 
 # Python program to demonstrate finally
 # No exception Exception raised in try block
-# try:
-# 	k = 5//0 # raises divide by zero exception.
-# 	print(k)
+try:
+	k = 5//0 # raises divide by zero exception.
+	print(k)
 
-# # handles zerodivision exception
-# except ZeroDivisionError:
-# 	print("Can't divide by zero")
+# handles zerodivision exception
+except ZeroDivisionError:
+	print("Can't divide by zero")
 
-# finally:
-# 	# this block is always executed
-# 	# regardless of exception generation.
-# 	print('This is always executed')
+finally:
+	# this block is always executed
+	# regardless of exception generation.
+	print('This is always executed')
 
 # Program to depict Raising Exception
 
